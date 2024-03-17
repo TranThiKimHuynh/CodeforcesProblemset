bc4da97de337e76f25baced53b550de16fca8d4d
TranThiKimHuynh <kimhuynhstudy12@gmail.com>
2024-03-17 23:24:04 +0700

    solve A. Again Twenty Five!

---
+++ A.Again_Twenty_Five.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+using namespace std;
+#define maxM 1000
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    // last two digits of 5^n is always 25
+    cout<<25<<endl;
+    return 0;
+}
