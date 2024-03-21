21b54a27405c0f0830e1eb057ea48ed03f66ba4a
TranThiKimHuynh <kimhuynhstudy12@gmail.com>
2024-03-21 23:42:46 +0700

    solve A. Black Square

---
+++ A.Black_Square.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+int main()
+{
+	int a[4];
+	cin>>a[0]>>a[1]>>a[2]>>a[3];
+	string s ;
+	cin>>s;
+	int sum =0;
+	for(int i = 0;i<s.length();i++){
+		sum+=a[s[i]-'0'-1];
+	}
+	cout<<sum<<endl;
+	return 0;
+}
+
