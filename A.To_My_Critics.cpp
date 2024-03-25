b03475771201aedcaadec4afbfaa9e82ac7a7634
TranThiKimHuynh <kimhuynhstudy12@gmail.com>
2024-03-25 22:26:38 +0700

    solve A. To My Critics

---
+++ A.To_My_Critics.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+      int a,b,c;
+	  cin>>a>>b>>c;
+	  if(a+b>=10 || b+c>=10 || a+c>=10){
+		  cout<<"YES"<<endl;
+	  }
+	  else{
+		  cout<<"NO"<<endl;
+	  }
+	}
+	return 0;
+}
