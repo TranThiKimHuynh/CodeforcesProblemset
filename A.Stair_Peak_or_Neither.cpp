32338dce3d505186946f50b596bc0320704e3672
TranThiKimHuynh <kimhuynhstudy12@gmail.com>
2024-03-29 22:39:35 +0700

    solve A. Stair, Peak, or Neither?

---
+++ A.Stair_Peak_or_Neither.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <math.h>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+int main()
+{   
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		if(a<b && b <c){
+			cout<<"STAIR"<<endl;
+		}
+		else if( a<b && b >c){
+			cout<<"PEAK"<<endl;
+		}
+		else{
+			cout<<"NONE"<<endl;
+		}
+	}
+
+	return 0;
+}
